9c17c49cdb14522bfa851117bd9484aeeecff05c
yasminbiaze <yasmin.b.martins@aluno.senai.br>
2025-07-31 10:31:34 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Bem vindo ao Mundo de Programação C#");
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("A soma é igual a: " + soma);
+
+    }
+}
